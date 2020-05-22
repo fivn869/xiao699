d7443bb0ae8d5cfacea88846d4f60906b9f8b4b1
Xiao Yan <yanxiao5211@gmail.com>
2020-05-22 19:02:29 +0100

    get the skill of implmentws the data

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import  views
 
 urlpatterns = [
-    path('hold_on/', views.post_list, name='post'),
-    path('getin/', views.post_test,name='test')
+    path('', views.post_list, name='post'),
+    path('123', views.post_test,name='test')
 ]
